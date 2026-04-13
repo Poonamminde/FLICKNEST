8fdfc8dea4c302ea036efc6806835163a8c900d8
Poonam Minde <poonamminde789@gmail.com>
2026-04-13 22:33:21 +0530

    chore: initial project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
